7bcea946e039818356ce21245a082f2b321c0abb
Arthur Holstvoogd <arthurholstvoogd@me.com>
2019-08-09 14:33:12 +0200

    Update fmt

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "container_definition" {
   value = jsonencode(
     {
-      name = var.name
-      image = var.image
-      hostname = var.hostname
-      cpu = var.cpu
-      memory = var.memory
+      name              = var.name
+      image             = var.image
+      hostname          = var.hostname
+      cpu               = var.cpu
+      memory            = var.memory
       memoryReservation = var.memory_reservation
-      essential = var.essential ? true : false
-      entryPoint = compact(split(" ", var.entrypoint))
-      command = compact(split(" ", var.command))
-      links = var.links
-      portMappings = var.port_mappings
-      mountPoints =  [for mount in var.mount_points : { "containerPath" = mount.path, "sourceVolume" = mount.volume }]
-      ulimits = [for name, limit in var.ulimits : { "name" = name, "softLimit" = limit }]
-      environment = [for name, value in var.environment_vars : { "name" = name, "value" = value }]
+      essential         = var.essential ? true : false
+      entryPoint        = compact(split(" ", var.entrypoint))
+      command           = compact(split(" ", var.command))
+      links             = var.links
+      portMappings      = var.port_mappings
+      mountPoints       = [for mount in var.mount_points : { "containerPath" = mount.path, "sourceVolume" = mount.volume }]
+      ulimits           = [for name, limit in var.ulimits : { "name" = name, "softLimit" = limit }]
+      environment       = [for name, value in var.environment_vars : { "name" = name, "value" = value }]
       logConfiguration = {
         logDriver = var.log_driver
-        options = var.log_driver_options
+        options   = var.log_driver_options
       }
-      tags = {}
+      tags        = {}
       volumesFrom = []
     }
   )
